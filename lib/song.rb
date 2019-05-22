c2970f42502b7eb22a3a474ff4b83baa087f8716
Conor Owens <conorj.owens@gmail.com>
2019-05-22 02:05:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,8 +7,8 @@ class Song
   end
   
   def new_by_filename(filename)
-    name = filename.split(" - ")[0]
-    artist = filename.split(/" - "|./)[1]
+    name = filename.split(" - ")[1]
+    artist = filename.split(" - ")[0]
     Song.new(name)
   end 
   
